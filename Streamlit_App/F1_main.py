ea29281585aa509a3e7533b6a3d4c55ce5e09e01
Arkevious Hardwick <42472558+ahardwick95@users.noreply.github.com>
2025-08-21 13:13:30 -0700

    Update F1_main.py

--- Streamlit_App/F1_main.py
+++ Streamlit_App/F1_main.py
@@ -7,7 +7,7 @@ from F5_Model_Comparisons import Model_Comparisons
 from F6_GenAI_SMS_Testing import Gen_AI_Model_Comparisons
 from F7_Future_Upgrades import Upgrades
 
-st.set_page_config(page_title="Spam Predictor App",layout="wide")
+st.set_page_config(page_title="PhishDetective",layout="wide")
 
 st.sidebar.title("📨 Navigation")
 selected_page = st.sidebar.selectbox("Choose a page:", ["About", "Spam Predictor","Spam Features","Model Comparisons","GenAI Testing","Future Upgrades"])
@@ -24,4 +24,5 @@ elif selected_page == "GenAI Testing":
     Gen_AI_Model_Comparisons()
 elif selected_page == "Future Upgrades":
     Upgrades()
+
     
